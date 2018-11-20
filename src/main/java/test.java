fc56b0d461f644560aeef7c7a430ba33a7ded0d6
Dmitry Pimushkin <dimapimushkin@mail.ru>
2018-11-20 18:34:17 +0400

    setting logger

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,12 +5,15 @@ public class test {
     private static final Logger logger = LogManager.getLogger(test.class);
 
     public static void main(String[] args) throws InterruptedException {
-
+            logger.info("Start application..");
         for (int i=0;i<1000;i++){
             logger.info("test counter for test log = "+i);
+            logger.info("test counter for test log {} "+i);
             logger.debug("[Debug] test counter for test log = {}",i);
+            logger.error("This error logger = {} {}",i,Thread.currentThread().toString());
             Thread.sleep(100);
         }
+        logger.info("End application..");
 
     }
 }
